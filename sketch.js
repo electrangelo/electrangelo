7e44436ae0eb13efbaeefe5fcfafcd03330d9473
p1nh0 <p1nh0.c0d1ng@gmail.com>
2024-10-15 16:31:40 +0100

    teste

--- sketch.js
+++ sketch.js
@@ -1,70 +1,26 @@
 /*
- * @name Particle System
- * @arialabel Light grey circles flowing out from a point like a sparkler 
- * @description This is a basic Particle System
- * (<a href="http://natureofcode.com">natureofcode.com</a>)
+ * @name Load and Play Sound
+ * @arialabel Red screen turns green when the user clicks on it and plays music
+ * @description Load sound during preload(). Play a sound when canvas is clicked.
+ * <br><br><em><span class="small"> To run this example locally, you will need the
+ * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
+ * a sound file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em>
  */
-let system;
+let song;
 
 function setup() {
-  createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  song = loadSound('guitarra.mp3');
+  createCanvas(720, 200);
+  background(255, 0, 0);
 }
 
-function draw() {
-  background(51);
-  system.addParticle();
-  system.run();
-}
-
-// A simple Particle class
-let Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255;
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(127, this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  return this.lifespan < 0;
-};
-
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
   }
-};
+}
